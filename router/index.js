62e18ab7ebe3da610a7593599f161ab37de5d9ff
alex <shizofranky@gmail.com>
2024-05-05 15:09:26 +0300

    refactor

--- router/index.js
+++ router/index.js
@@ -1,7 +1,7 @@
 const { Router, response } = require("express");
 const { checkIfUserExists } = require("../middleware");
 
-const jwt = require('jsonwebtoken') ;
+const jwt = require("jsonwebtoken");
 
 const router = new require("express").Router();
 
@@ -36,37 +36,36 @@ router.post("/login", async (request, response) => {
   const body = request.body;
 
   if (!body)
-    return response
-      .status(418)
-      .json({
-        status: false,
-        message: "the body object could not be parsed",
-        payload: null,
-      });
+    return response.status(418).json({
+      status: false,
+      message: "the body object could not be parsed",
+      payload: null,
+    });
 
   const { password, email } = body;
 
   if (!password || !email)
-    return response
-      .status(401)
-      .json({
-        status: false,
-        message: " you must provide both your PASSWORD and EMAIL address",
-        payload: null,
-      });
+    return response.status(401).json({
+      status: false,
+      message: " you must provide both your PASSWORD and EMAIL address",
+      payload: null,
+    });
 
   const usersCollection = collection(fireStore, "users");
 
   const usersSnapshot = await getDocs(usersCollection);
 
-  const users = usersSnapshot.docs.map((elem) => ({...elem.data() , id:elem.id}));
+  const users = usersSnapshot.docs.map((elem) => ({
+    ...elem.data(),
+    id: elem.id,
+  }));
   // const users = usersSnapshot.docs.map((elem) => elem.);
 
   if (!users.length)
-    return customResponse(response, 400 , false, "that user not registred", {
+    return customResponse(response, 400, false, "that user not registred", {
       content: {
-        type:'users_list' ,
-        data:users
+        type: "users_list",
+        data: users,
       },
       subject: null,
     });
@@ -74,10 +73,10 @@ router.post("/login", async (request, response) => {
   const usersByEmail = users.filter((elem) => elem.email === email);
 
   if (!usersByEmail.length)
-    return customResponse(response, 400 , false, "wrong email", {
+    return customResponse(response, 400, false, "wrong email", {
       content: {
-        type:'users_list' ,
-        data:users
+        type: "users_list",
+        data: users,
       },
       subject: "email",
     });
@@ -87,46 +86,63 @@ router.post("/login", async (request, response) => {
   );
 
   if (!userFiltredByPassword.length)
-    return customResponse(response, 400 , false, "wrong password", {
+    return customResponse(response, 400, false, "wrong password", {
       content: {
-        type:'users_list' ,
-        data:users
+        type: "users_list",
+        data: users,
       },
       subject: "password",
     });
 
+  if (userFiltredByPassword.length > 1)
+    return customResponse(response, 500, false, "enternal error", null);
 
-  if(userFiltredByPassword.length > 1) 
-    return customResponse(response, 500 , false, "enternal error", null);
+  const SECRET_KEY = process.env.SECRET_KEY || "MY-SECRET-KEY";
 
-  const SECRET_KEY = process.env.SECRET_KEY || "MY-SECRET-KEY" ;
-
-  const token = jwt.sign({userID:userFiltredByPassword[0].id} , SECRET_KEY , {
-    expiresIn:"10s" ,
+  const token = jwt.sign({ userID: userFiltredByPassword[0].id }, SECRET_KEY, {
+    expiresIn: "10s",
     // algorithm:'ES256'
   });
 
-  return customResponse(response, 200 , true, "it s okay , u authorized , catch the token", {
-    content: {
-      type:'access_token' ,
-      data:token
+  return customResponse(
+    response,
+    200,
+    true,
+    "it s okay , u authorized , catch the token",
+    {
+      content: {
+        type: "access_token",
+        data: token,
+      },
+      subject: null,
     },
-    subject: null,
-  });
+  );
 });
 
 router.post("/registration", checkIfUserExists, async (request, response) => {
   const body = request.body;
 
   if (!body)
-    return customResponse(response, 209, false ,  "sorry , you have no body", null);
+    return customResponse(
+      response,
+      209,
+      false,
+      "sorry , you have no body",
+      null,
+    );
 
   const { username, email, password } = body;
 
   console.log({ username, email, password });
 
   if (!password || password == "" || password == " ")
-    return customResponse(response, 509, false , "sorry , you have NO PASSWORD", null);
+    return customResponse(
+      response,
+      509,
+      false,
+      "sorry , you have NO PASSWORD",
+      null,
+    );
 
   const userCollection = collection(fireStore, "users");
 
@@ -138,7 +154,13 @@ router.post("/registration", checkIfUserExists, async (request, response) => {
 
   console.log({ addedResult: addedResult.id });
 
-  return customResponse(response, 209, true , "user registered successful", null);
+  return customResponse(
+    response,
+    209,
+    true,
+    "user registered successful",
+    null,
+  );
 });
 
 /**
@@ -152,7 +174,7 @@ router.post("/registration", checkIfUserExists, async (request, response) => {
  * @returns { void }
  */
 
-function customResponse(response, htttpStatus , customStatus, message, payload) {
+function customResponse(response, htttpStatus, customStatus, message, payload) {
   const data = {
     status: customStatus,
     message,
@@ -163,6 +185,6 @@ function customResponse(response, htttpStatus , customStatus, message, payload) 
 }
 
 module.exports = {
-  router ,
-  customResponse ,
-}
+  router,
+  customResponse,
+};
